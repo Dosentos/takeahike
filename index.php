dc0ff9d325163f0662ee6a181a51a8662b1fab60
Dosentos <ketola.viljami@gmail.com>
2017-04-10 20:04:23 +0300

    Lisätty sivurakenteen peruselementit ja CDN:t
    PHP tiedostot
    -config.php
    -footer.php
    -frontpage.php
    -header.php
    -index.php
    
    JS kansio
    -test.js (ei toimi tällä hetkellä)
    
    CDN:t
    -Foundation js & css
    -React & React-dom
    -Babel

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dosentti
+ * Date: 10.4.2017
+ * Time: 14.31
+ */
+//require_once(config.php);
+
+include('header.php');
+include('frontpage_content.php');
+include('footer.php');
+?>
