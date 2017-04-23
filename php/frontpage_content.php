f20260685219ae04bf3afe8466a5b2e766882528
Duukkis8D <duukkis@gmail.com>
2017-04-23 19:07:51 +0300

    Google Maps test

--- php/frontpage_content.php
+++ php/frontpage_content.php
@@ -8,10 +8,25 @@
 ?>
 
 <main>
-    <h3>Front page!</h3>
-    <div id="test-div"></div>
+    <h3>Destinations in Finland</h3>
+    <!-- API key Google Maps Javascript API:in: AIzaSyDXvGjS2pVvCg1fac8IjYXnyFob7FUqoMs -->
 
-    <!-- Hello world reactilla -->
-    <script src="js/test.js" type="text/babel"> </script>
+    <div id="map"></div>
+    <script>
+        function initMap() {
+            var uluru = {lat: -25.363, lng: 131.044};
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 4,
+                center: uluru
+            });
+            var marker = new google.maps.Marker({
+                position: uluru,
+                map: map
+            });
+        }
+    </script>
+    <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDXvGjS2pVvCg1fac8IjYXnyFob7FUqoMs&callback=initMap">
+    </script>
 </main>
 
