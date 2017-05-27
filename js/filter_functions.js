497f06be2f3f511dbe364ce1b5d4b5c28e6306e5
Duukkis8D <duukkis@gmail.com>
2017-05-27 20:23:00 +0300

    Location filters for geographical data, not working

--- js/filter_functions.js
+++ js/filter_functions.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
     // Tähän koodia, jolla saa valittujen sijaintifiltterien perusteella oikeat markkerit kartalle.
     var inputValues = {};
     $( ".locationFilter:checkbox:checked" ).each( function( index ) {
-        inputValues[ index.$( this ).val() ];
+
     });
-    alert( inputValues );
+    //alert( inputValues );
     //$.get( "generate_marker_xml.php", inputValues );
 });
